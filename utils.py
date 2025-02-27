82482dd750b7b63138ad4ee1488b4981b4fcdbbc
RuurdKuiper <46319514+RuurdKuiper@users.noreply.github.com>
2025-02-27 08:49:41 +0000

    - Change LLM to GPT-4o

--- utils.py
+++ utils.py
@@ -20,7 +20,7 @@ def get_chatgpt_response(prompt, pdf_text):
     client = OpenAI()
 
     completion = client.chat.completions.create(
-    model="gpt-4o-mini",
+    model="gpt-4o",
     messages=[
         {"role": "system", "content": f"""You are a helpful assistant. 
          
